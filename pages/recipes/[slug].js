13e6799bb86bd046ef3c96de5462e9b4edf5e369
Hoang <jasonkindo1710@gmail.com>
2022-08-28 13:21:21 +0700

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -27,6 +27,15 @@ export const getStaticProps = async ({ params }) => {
     content_type: "recipe",
     "fields.slug": params.slug,
   });
+
+  if(!items.length){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
   return {
     props: {
       recipe: items[0],
