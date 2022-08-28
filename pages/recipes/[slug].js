4d5661734873434acf6e809943a91e63f68d650a
Hoang <jasonkindo1710@gmail.com>
2022-08-28 13:28:52 +0700

    404

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -19,7 +19,7 @@ export const getStaticPaths = async () => {
   });
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 export const getStaticProps = async ({ params }) => {
